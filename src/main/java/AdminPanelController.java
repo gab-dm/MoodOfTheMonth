cdcc92f3124a71bcd0c126cce6f3a7d2d3182ce7
Steven Tien <stien@takima.fr>
2023-02-07 14:37:52 +0100

    Add admin panel (/adminPanel) + link to add user

---
+++ src/main/java/AdminPanelController.java
@@ -0,0 +1,24 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/adminPanel")
+public class AdminPanelController extends HttpServlet {
+
+    @Override
+    public void init() {
+        System.out.println("Servlet initialized successfully");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/admin-panel.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
