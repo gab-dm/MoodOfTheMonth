a447ad8c99690742c8e544ee12a714a11b8b160c
Tom KAUFFMANN <tkauffmann@takima.fr>
2023-02-08 16:30:21 +0100

    updated gitignore

--- src/main/script_creation_SQL.sql
+++ src/main/script_creation_SQL.sql
@@ -12,8 +12,8 @@ CREATE TABLE moods (
     comment varchar (200)
 );
 
-INSERT INTO employees (name, email, birthdate) VALUES ('Tom Kauffmann', 'tkauffmann@takima.fr', '23-04-1999');
+INSERT INTO employees (name, email, birthdate) VALUES ('Tom Kauffmann', 'tkauffmann@takima.fr', '1999-04-23');
 INSERT INTO employees (name, email, birthdate) VALUES ('Steven Tien', 'stien@takima.fr', '05-08-1999');
-INSERT INTO employees (name, email, birthdate) VALUES ('Gabriel Demontcheuil', 'gdemontcheuil@takima.fr', '19-02-1999');
+INSERT INTO employees (name, email, birthdate) VALUES ('Gabriel Demontcheuil', 'gdemontcheuil@takima.fr', '1998-02-19');
 
 INSERT INTO moods (mark, name, comment) VALUES (1, 'Gabriel Demontcheuil', 'ceci est un test de commentaire random : hibernate on te deteste');
